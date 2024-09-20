174a9ed3d83b89c9ceaa09ded9f3fd1aa4e67af9
Freeagles1991 <dmitry.nerovny@gmail.com>
2024-09-20 20:10:38 +0300

    Добавил скриншот-тесты

--- TrackerTests/TrackerTests.swift
+++ TrackerTests/TrackerTests.swift
@@ -1,36 +1,17 @@
-//
-//  TrackerTests.swift
-//  TrackerTests
-//
-//  Created by Дима on 25.07.2024.
-//
-
 import XCTest
+import SnapshotTesting
+import SwiftUI
+
 @testable import Tracker
 
-final class TrackerTests: XCTestCase {
-
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+final class SnapshotTests: XCTestCase {
+    
+    func testTrackerMainScreenSnapshot() {
+        // Given
+        let viewController = TrackersViewController()
+        viewController.loadViewIfNeeded()
+        
+        // Then
+        assertSnapshot(of: viewController, as: .image(on: .iPhone13))
     }
-
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
-
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        // Any test you write for XCTest can be annotated as throws and async.
-        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
-        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
-    }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
-    }
-
 }
